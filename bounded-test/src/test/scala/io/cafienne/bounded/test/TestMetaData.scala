83c8e30efb40b8f0f411741d2706a8c3c0e7dd52
Her <hervandevliert@gmail.com>
2018-09-06 16:16:34 +0200

    Changed CommandMetadata / MetaData to a trait to allow users of the framework to add new properties

---
+++ bounded-test/src/test/scala/io/cafienne/bounded/test/TestMetaData.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016-2018 Cafienne B.V. <https://www.cafienne.io/bounded>
+ */
+
+package io.cafienne.bounded.test
+
+import java.time.ZonedDateTime
+import java.util.UUID
+
+import io.cafienne.bounded.aggregate.{CommandMetaData, MetaData}
+import io.cafienne.bounded.{BuildInfo, RuntimeInfo, UserContext}
+
+case class TestCommandMetaData(
+  timestamp: ZonedDateTime,
+  val userContext: Option[UserContext],
+  override val commandId: UUID = UUID.randomUUID()
+) extends CommandMetaData
+
+case class TestMetaData(
+  timestamp: ZonedDateTime,
+  userContext: Option[UserContext],
+  causedByCommand: Option[UUID],
+  buildInfo: BuildInfo,
+  runTimeInfo: RuntimeInfo
+) extends MetaData
+
+object TestMetaData {
+  def fromCommand(
+    metadata: TestCommandMetaData
+  )(implicit buildInfo: BuildInfo, runtimeInfo: RuntimeInfo): TestMetaData = {
+    TestMetaData(
+      metadata.timestamp,
+      metadata.userContext,
+      Some(metadata.commandId),
+      buildInfo,
+      runtimeInfo
+    )
+  }
+}
