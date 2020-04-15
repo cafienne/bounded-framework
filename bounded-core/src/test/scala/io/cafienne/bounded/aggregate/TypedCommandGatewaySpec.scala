d2bf50a8da818f9eb50f8b20263f74f3b271d061
Olger Warnier <olger@spectare.nl>
2020-04-15 13:48:57 +0200

    Typed commandgateway test works with whenReady. On sleep left to ensure handling of events on other threads. Keep logging hidden until it fails. (LogCapturing option of akka log testkit)

--- bounded-core/src/test/scala/io/cafienne/bounded/aggregate/TypedCommandGatewaySpec.scala
+++ bounded-core/src/test/scala/io/cafienne/bounded/aggregate/TypedCommandGatewaySpec.scala
@@ -6,12 +6,16 @@ package io.cafienne.bounded.aggregate
 
 import java.time.ZonedDateTime
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.testkit.typed.javadsl.LoggingTestKit
+import akka.actor.testkit.typed.scaladsl.{LogCapturing, ManualTime, ScalaTestWithActorTestKit}
+import akka.actor.typed.ActorSystem
 import akka.util.Timeout
 import io.cafienne.bounded.{BuildInfo, RuntimeInfo}
 import io.cafienne.bounded.aggregate.typed.DefaultTypedCommandGateway
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AsyncFlatSpecLike
+
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
@@ -28,7 +32,7 @@ class TypedCommandGatewaySpec extends ScalaTestWithActorTestKit(s"""
     akka.coordinated-shutdown.run-by-actor-system-terminate = off
     akka.coordinated-shutdown.run-by-jvm-shutdown-hook = off
     akka.cluster.run-coordinated-shutdown-when-down = off
-    """) with AsyncFlatSpecLike with BeforeAndAfterAll {
+    """) with AsyncFlatSpecLike with ScalaFutures with BeforeAndAfterAll with LogCapturing {
   //NOTE: Be aware that the config of the TestKit contains the name of the Cluster based on the name of this Spec (TypedClusteredSpec).
   //      When you copy this for your own use, change the name of the cluser.seed-nodes.
 
@@ -39,8 +43,9 @@ class TypedCommandGatewaySpec extends ScalaTestWithActorTestKit(s"""
       Future.successful(cmd)
   }
 
-  implicit val ec        = system.executionContext
-  implicit val scheduler = system.scheduler
+  implicit val actorSystem: ActorSystem[_] = system
+  implicit val ec                          = system.executionContext
+  implicit val scheduler                   = system.scheduler
 
   behavior of "Typed Command Gateway"
 
@@ -56,50 +61,55 @@ class TypedCommandGatewaySpec extends ScalaTestWithActorTestKit(s"""
     val commandMetaData = AggregateCommandMetaData(ZonedDateTime.now(), None)
     val aggregateId     = "test0"
     val probe           = testKit.createTestProbe[Response]()
-    Await.result(typedCommandGateway.send(Create(aggregateId, commandMetaData, probe.ref)), 2.seconds) shouldEqual (())
-    val probeAnswer = probe.expectMessage(OK)
-    assert(probeAnswer.equals(OK))
+    whenReady(typedCommandGateway.send(Create(aggregateId, commandMetaData, probe.ref))) { answer =>
+      answer shouldEqual (())
+      val probeAnswer = probe.expectMessage(OK)
+      assert(probeAnswer.equals(OK))
+    }
+
   }
 
   "Command Gateway" should "handle second message via actor in the Map" in {
     val commandMetaData = AggregateCommandMetaData(ZonedDateTime.now(), None)
     val aggregateId     = "test0"
     val probe           = testKit.createTestProbe[Response]()
-    Await.result(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 1", probe.ref)), 2.seconds) shouldEqual (())
-    val probeAnswer = probe.expectMessage(OK)
-    assert(probeAnswer.equals(OK))
+    whenReady(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 1", probe.ref))) { answer =>
+      answer shouldEqual (())
+      val probeAnswer = probe.expectMessage(OK)
+      assert(probeAnswer.equals(OK))
+    }
   }
 
   "Command Gateway" should "recover after stop message and add to the map" in {
     val commandMetaData = AggregateCommandMetaData(ZonedDateTime.now(), None)
     val aggregateId     = "test0"
     val probe           = testKit.createTestProbe[Response]()
-    Await.result(typedCommandGateway.send(Stop(aggregateId, commandMetaData, probe.ref)), 2.seconds) shouldEqual (())
-    probe.expectMessage(OK)
+    whenReady(typedCommandGateway.send(Stop(aggregateId, commandMetaData, probe.ref))) { answer =>
+      answer shouldEqual (())
+      probe.expectMessage(OK)
+    }
     Thread.sleep(3000)
-    Await.result(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 2", probe.ref)), 2.seconds) shouldEqual (())
-    val probeAnswer = probe.expectMessage(OK)
-    assert(probeAnswer.equals(OK))
+    whenReady(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 2", probe.ref))) { answer =>
+      answer shouldEqual (())
+      val probeAnswer = probe.expectMessage(OK)
+      assert(probeAnswer.equals(OK))
+    }
   }
 
   "Command Gateway" should "recover after the aggregate stopped itself" in {
-    //TODO this test is not finished. The recover after an internal aggregate stop is not implemented at this moment.
-    //1) find a way to stop an aggregate not directly responding on a command
-    //2) send a command that will cause the stopping to the aggregate
-    //3) See if stopping involves the AggregateTerminated message or the normal Terminated message
-    //CONCLUSION TILL NOW: AggregateTerminated is called when the actor stops internally
-    //4) Send a new command to the aggregate, at this moment when the normal Terminated was used the actor ref is dead
-    //   and there will be a dead letter.
     val commandMetaData = AggregateCommandMetaData(ZonedDateTime.now(), None)
     val aggregateId     = "test0"
     val probe           = testKit.createTestProbe[Response]()
-    Await.result(typedCommandGateway.send(StopAfter(aggregateId, commandMetaData, 3, probe.ref)), 2.seconds) shouldEqual (())
-    val probeAnswer = probe.expectMessage(OK)
-    assert(probeAnswer.equals(OK))
-    Thread.sleep(3000)
-    Await.result(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 3", probe.ref)), 2.seconds) shouldEqual (())
-    val probeAnswer2 = probe.expectMessage(OK)
-    assert(probeAnswer2.equals(OK))
+    whenReady(typedCommandGateway.send(StopAfter(aggregateId, commandMetaData, 3, probe.ref))) { answer =>
+      answer shouldEqual (())
+      val probeAnswer = probe.expectMessage(OK)
+      assert(probeAnswer.equals(OK))
+    }
+    whenReady(typedCommandGateway.send(AddItem(aggregateId, commandMetaData, "new item 3", probe.ref))) { answer =>
+      answer shouldEqual (())
+      val probeAnswer2 = probe.expectMessage(OK)
+      assert(probeAnswer2.equals(OK))
+    }
   }
 
   protected override def afterAll(): Unit = {
